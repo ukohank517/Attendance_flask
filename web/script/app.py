0019a04c445b2695778adf32ad24eb7d3035b43a
ukohank517 <ukohank517@gmail.com>
2020-09-27 19:11:04 +0900

    [update]  サンプルフォーム

--- web/script/app.py
+++ web/script/app.py
@@ -1,6 +1,6 @@
 # coding=utf-8
 import os
-from flask import Flask, jsonify, request
+from flask import Flask, Blueprint, jsonify, request, render_template
 import MySQLdb
 
 app = Flask(__name__)
@@ -9,16 +9,17 @@ app.config.from_pyfile(flask_conf_file)
 mail_file = os.path.join(os.getcwd(), 'data', 'personal_info.csv')
 
 
+@app.route('/entry', methods=['GET'])
+def test():
+    rest_seats = 10
+    return render_template('entry.html', rest_seats=rest_seats)
 
 @app.route('/', methods=['GET'])
 def hello():
     target_prefecture = request.args.get('pref')
 
-    conn = MySQLdb.connect(user='root', passwd='pass', host='db_server', db='attendance')
-    cur = conn.cursor()
     sql = "select * from personal_info;"
-    cur.execute(sql)
-    rows = cur.fetchall()
+    rows = search_query(sql)
 
     result_dict = {}
     mail_address_list = []
@@ -33,6 +34,12 @@ def hello():
 
     result_dict['mail_address_list'] = mail_address_list
     return jsonify(result_dict)
+
+def search_query(sql):
+    conn = MySQLdb.connect(user='root', passwd='pass', host='db_server', db='attendance')
+    cur = conn.cursor()
+    cur.execute(sql)
+    return cur.fetchall()
 
 
 if __name__ == '__main__':
